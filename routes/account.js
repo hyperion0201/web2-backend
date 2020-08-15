3bc723abd2425a8a46cfa13af4776108c3188645
Hieu Hoang <megamind.ft@gmail.com>
2020-08-15 16:11:50 +0700

    update api charge account

--- routes/account.js
+++ routes/account.js
@@ -67,4 +67,32 @@ router.post(
     });
   }
 );
+router.post(
+  "/charge",
+  passport.authenticate("jwt", {
+    session: false,
+  }),
+  async (req, res) => {
+    const user = _.get(req, "user.dataValues");
+    const account_id = _.get(req, "body.account_id", null);
+    const amount = _.get(req, "body.amount", 0);
+    const accountFound = await Account.findAccount(account_id);
+    if (!accountFound) {
+      return res.status(400).send({
+        error: "Account not found.",
+      });
+    }
+    // nap tien xd
+    await Account.updateAccount({
+      account_id,
+      accountData: {
+        account_balance: accountFound.account_balance + parseFloat(amount),
+      },
+    });
+
+    res.json({
+      message: "Successfully charge account.",
+    });
+  }
+);
 module.exports = router;
